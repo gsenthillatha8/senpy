826a23e91a2ceae9a7e421fc9486586b86144499
Senthil Kumar Ganesan <senthilkumarganesan@Senthils-MacBook-Pro-2.local>
2025-02-16 18:48:39 -0700

    My first commit

---
+++ data_type_str.py
@@ -0,0 +1,20 @@
+print(dir('st'))
+
+'''
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', 
+'__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__',
+'__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', 
+'__str__', '__subclasshook__', 
+
+>> 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
+'''
+print(''.capitalize) # <built-in method capitalize of str object at 0x10e33eb00>
+print(''.capitalize.__doc__)
+'''
+Return a capitalized version of the string.
+
+More specifically, make the first character have upper case and the rest lower
+case.
+'''
+print('senThil'.capitalize()) # Senthil
---
+++ data_types.py
@@ -0,0 +1,15 @@
+"""
+This module will demonstrate that python is an interpereted weekly typed language
+"""
+i=1
+print(type(i)) # <class 'int'>
+i='string'
+print(type(i)) # <class 'str'>
+i=1.0
+print(type(i)) # <class 'float'>
+i =[1,23,4]
+print(type(i)) # <class 'list'>
+i={'name':"Senthil"}
+print(type(i)) # <class 'dict'>
+
+
